5da92468c4ff19db8e8383114b61a763c3ace855
Vincent Bean <github@vincentbean.com>
2025-11-09 21:45:08 +0100

    Adjust tests

--- src/BuildResponse.php
+++ src/BuildResponse.php
@@ -9,26 +9,20 @@ class BuildResponse
 {
     public function build(
         array $checks,
-        array $metricChecks
+        array $metrics
     ): array {
-        /** @var array<int, Check> $checks */
         $checkResults = [];
-        /** @var array<int, Metric> $metricChecks */
         $metricResults = [];
 
         foreach ($checks as $check) {
-            $instance = $check->build();
-
-            if ($instance->available() && $instance instanceof Check) {
-                $checkResults[] = $instance->run()->toArray();
+            if ($check->available() && $check instanceof Check) {
+                $checkResults[] = $check->run()->toArray();
             }
         }
 
-        foreach ($metricChecks as $metricCheck) {
-            $instance = $metricCheck->build();
-
-            if ($instance->available() && $instance instanceof Metric) {
-                $metricResults[] = $instance->measure()->toArray();
+        foreach ($metrics as $metric) {
+            if ($metric->available() && $metric instanceof Metric) {
+                $metricResults[] = $metric->measure()->toArray();
             }
         }
 
